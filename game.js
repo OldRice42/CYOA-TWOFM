67f6bfa5797a429b809a1715970b4f911b295a16
Jessica Hylton <jessica.hylton42@gmail.com>
2021-06-08 21:23:10 -0400

    Added a dice roller. The second one does not work. Will try to write something that might allow for two dice roll buttons.

--- game.js
+++ game.js
@@ -40,6 +40,67 @@ function selectOption(option) {
   showTextNode(nextTextNodeId)
 }
 
+
+//Example roller
+var dice = {
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+
+
+//Prints dice roll to the page
+
+function printNumber(number) {
+  var placeholder = document.getElementById('placeholder');
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
+
+
+/*
+//Second dice
+var dicetwo = {
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+
+
+//Prints dice roll to the page
+
+function printNumber(number) {
+  var placeholdertwo = document.getElementById('placeholdertwo');
+  placeholdertwo.innerHTML = number;
+}
+
+var buttontwo = document.getElementById('buttontwo');
+
+buttontwo.onclick = function() {
+  var result = dicetwo.roll();
+  printNumber(result);
+};
+*/
+
+
 const textNodes = [
   {
     id: 1,
